981061e39271e50fea0da1d19fab07a4ae0a0744
siddharth.aagnihotri@codelovertechnology.com <siddharth.aagnihotri@codelovertechnology.com>
2021-05-13 18:22:42 +0530

    changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,18 +4,17 @@ import logo from '../src/image/logo.png';
 const Navbar = () =>{
     return(
         <>
-<div className="container-fluid nav_bg ">
- <div className="row">
-    <div className="col-10 mx-auto">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to='/' ><img src={logo} alt="logo" className="img-fluid" /></NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav  mb-2 mb-lg-0 ">
-      <li className="nav-item">
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+  <NavLink className="navbar-brand" to="/"><img src={logo} alt="Logo" /></NavLink>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto w-100 justify-content-end">
+    <li className="nav-item">
           <NavLink activeClassName='menu_active' exact className="nav-link" to='/about' >About</NavLink>
         </li>
         <li className="nav-item">
@@ -31,13 +30,11 @@ const Navbar = () =>{
         <li className="nav-item">
           <NavLink activeClassName='menu_active' exact className="nav-link" to='/contact' >Contact</NavLink>
         </li>
-      </ul>
-    </div>
+    </ul>
+  </div>
   </div>
 </nav>
-                    </div>
-                </div>
-            </div>
+</header>
         </>
     );
 };
